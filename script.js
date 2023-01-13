add187defbe98f54ffdd8bbcaf6350956bef3e37
Nicolas Fuentes <nfuentes11@gmail.com>
2023-01-12 23:55:40 -0500

    Funcition add that turns the div's balck and back to red

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ for (let i = 0; i < 256; i++) {
   square.classList.add("squares");
   grid.appendChild(square);
 }
-// fix this event 
-grid.addEventListener('mouseenters', event =>{
-    event.target.style.backgroundColor ='solid balck'
-})
-
+// fix this even
+const divEvent = document.getElementsByClassName("squares");
+        
+        for(let i=0; i< divEvent.length; i++){
+            divEvent[i].addEventListener('click',function(){
+                if(this.style.backgroundColor === 'black'){
+                    this.style.backgroundColor= 'red';
+                }else{
+                    this.style.backgroundColor= 'black';
+                }
+            });
+        }
+          
+          
+          
+        // for(let i=0; i< divEvent.length; i++){
+        //     divEvent[i].addEventListener("mousedown",function(){
+        //         this.style.backgroundColor= 'black'
+                
+        //     });
+        // }
