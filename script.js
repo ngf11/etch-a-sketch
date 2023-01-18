2731b3a542ead65f2bed7896381b798d12106e31
Nicolas Fuentes <nfuentes11@gmail.com>
2023-01-17 22:55:08 -0500

    Basic functionalities of the sketch-pad finshed. Need to add a random RGB button and full UI.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let grid = document.getElementById("sketchGrid");
-
-for (let i = 0; i < 256; i++) {
+let gridSize = 16;
+for (let i = 0; i < gridSize * gridSize; i++) {
   const square = document.createElement("div");
   square.classList.add("squares");
   grid.appendChild(square);
@@ -17,7 +17,8 @@ const divEvent = document.getElementsByClassName("squares");
                     this.style.backgroundColor= 'black';
                 }
             });
-        }
+       }
+       
           
           const eraseButton = document.getElementById('erase');
                 eraseButton.addEventListener('click', event =>{
@@ -27,5 +28,36 @@ const divEvent = document.getElementsByClassName("squares");
 
                 });
 
-
-
+                const newGridButton = document.getElementById("new-grid");
+                newGridButton.addEventListener("click", event => {
+                  gridSize = prompt("Enter number of squares per side (up to 100) for the new grid:");
+                  // validate user input
+                  if (gridSize > 100) {
+                    gridSize = 100;
+                  }
+                  if (gridSize < 1) {
+                    gridSize = 1;
+                  }
+                  // remove existing grid
+                  grid.innerHTML = "";
+                  // create new grid
+                  for (let i = 0; i < gridSize * gridSize; i++) {
+                    const square = document.createElement("div");
+                    square.classList.add("squares");
+                    square.style.width = `${960 / gridSize}px`;
+                    square.style.height = `${480 / gridSize}px`;
+                    grid.appendChild(square);
+                  }
+                  // add event listeners to new squares
+                  const divEvent = document.getElementsByClassName("squares");
+                  for (let i = 0; i < divEvent.length; i++) {
+                    divEvent[i].addEventListener("click", function() {
+                      if (this.style.backgroundColor === "black") {
+                        this.style.backgroundColor = "whitesmoke";
+                      } else {
+                        this.style.backgroundColor = "black";
+                      }
+                    });
+                  }
+                });
+                
